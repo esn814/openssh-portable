5c521f6f7bffd91a5324b128de2c2bde3a225ecf
Andrew <anmenaga@microsoft.com>
2023-11-30 11:47:21 -0800

    Removed sha1-based MACs from default config

--- myproposal.h
+++ myproposal.h
@@ -68,12 +68,10 @@
 	"umac-128-etm@openssh.com," \
 	"hmac-sha2-256-etm@openssh.com," \
 	"hmac-sha2-512-etm@openssh.com," \
-	"hmac-sha1-etm@openssh.com," \
 	"umac-64@openssh.com," \
 	"umac-128@openssh.com," \
 	"hmac-sha2-256," \
-	"hmac-sha2-512," \
-	"hmac-sha1"
+	"hmac-sha2-512,"
 
 #define KEX_CLIENT_MAC KEX_SERVER_MAC
 
